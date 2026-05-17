456862c8cfaca9f35f422e6516e9ef4a2db5f3c8
riazbd <nuruddinriaz5@gmail.com>
2026-05-17 17:27:34 +0600

    feat: add bangladesh cities config with lat/lng

---
+++ config/bangladesh_cities.php
@@ -0,0 +1,22 @@
+<?php
+// config/bangladesh_cities.php
+return [
+    'default' => 'dhaka',
+
+    'cities' => [
+        'dhaka'       => ['name_en' => 'Dhaka',        'name_bn' => 'ঢাকা',         'lat' => 23.8103,  'lng' => 90.4125,  'country' => 'BD'],
+        'chittagong'  => ['name_en' => 'Chittagong',   'name_bn' => 'চট্টগ্রাম',    'lat' => 22.3569,  'lng' => 91.7832,  'country' => 'BD'],
+        'sylhet'      => ['name_en' => 'Sylhet',       'name_bn' => 'সিলেট',         'lat' => 24.8949,  'lng' => 91.8687,  'country' => 'BD'],
+        'rajshahi'    => ['name_en' => 'Rajshahi',     'name_bn' => 'রাজশাহী',       'lat' => 24.3745,  'lng' => 88.6042,  'country' => 'BD'],
+        'khulna'      => ['name_en' => 'Khulna',       'name_bn' => 'খুলনা',         'lat' => 22.8456,  'lng' => 89.5403,  'country' => 'BD'],
+        'barisal'     => ['name_en' => 'Barisal',      'name_bn' => 'বরিশাল',        'lat' => 22.7010,  'lng' => 90.3535,  'country' => 'BD'],
+        'rangpur'     => ['name_en' => 'Rangpur',      'name_bn' => 'রংপুর',         'lat' => 25.7439,  'lng' => 89.2752,  'country' => 'BD'],
+        'mymensingh'  => ['name_en' => 'Mymensingh',   'name_bn' => 'ময়মনসিংহ',     'lat' => 24.7471,  'lng' => 90.4203,  'country' => 'BD'],
+        'comilla'     => ['name_en' => 'Comilla',      'name_bn' => 'কুমিল্লা',      'lat' => 23.4607,  'lng' => 91.1809,  'country' => 'BD'],
+        'narayanganj' => ['name_en' => 'Narayanganj',  'name_bn' => 'নারায়ণগঞ্জ',   'lat' => 23.6238,  'lng' => 90.4996,  'country' => 'BD'],
+        'gazipur'     => ['name_en' => 'Gazipur',      'name_bn' => 'গাজীপুর',       'lat' => 23.9999,  'lng' => 90.4203,  'country' => 'BD'],
+        'jessore'     => ['name_en' => 'Jessore',      'name_bn' => 'যশোর',          'lat' => 23.1667,  'lng' => 89.2167,  'country' => 'BD'],
+        'bogra'       => ['name_en' => 'Bogra',        'name_bn' => 'বগুড়া',        'lat' => 24.8465,  'lng' => 89.3776,  'country' => 'BD'],
+        'coxsbazar'   => ['name_en' => "Cox's Bazar",  'name_bn' => "কক্সবাজার",    'lat' => 21.4272,  'lng' => 92.0058,  'country' => 'BD'],
+    ],
+];
