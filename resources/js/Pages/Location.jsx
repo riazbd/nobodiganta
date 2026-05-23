9cc1c040ab10867535c4d82912f2c7b1da4771d6
riazbd <nuruddinriaz5@gmail.com>
2026-05-23 17:32:43 +0600

    addrs search icon in search button

--- resources/js/Pages/Location.jsx
+++ resources/js/Pages/Location.jsx
@@ -280,6 +280,9 @@ function LocationFilterWidget({ division: initDiv, district: initDist, upazila: 
       </select>
 
       <button className="loc-widget-btn" onClick={handleSearch}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" style={{ display:'inline-block', verticalAlign:'middle', marginRight:7, marginTop:-2 }}>
+          <circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/>
+        </svg>
         {lang === 'bn' ? 'খুঁজুন' : 'Search'}
       </button>
     </div>
