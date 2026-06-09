b4d1eb59675e83389dc2ad863e1913e3b6098df7
riazbd <nuruddinriaz5@gmail.com>
2026-06-09 16:09:05 +0600

    added fixes of scroll to top

--- resources/js/Components/BackToTop.jsx
+++ resources/js/Components/BackToTop.jsx
@@ -10,6 +10,10 @@ export default function BackToTop() {
   }, []);
 
   return (
-    <button id="btt" className={visible ? 'show' : ''} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>▲</button>
+    <button id="btt" className={visible ? 'show' : ''} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} aria-label="Scroll to top">
+      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M3 10.5L8 5.5L13 10.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      </svg>
+    </button>
   );
 }
